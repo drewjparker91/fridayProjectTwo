9eee90b288a27b8de6fa39e2e50e78660e429cf7
drewparker <drewjparker91@gmail.com>
2020-08-14 10:32:52 -0700

    fix else if bug

--- js/scripts.js
+++ js/scripts.js
@@ -9,58 +9,59 @@ $(document).ready(function() {
   if (businessSize === "smallBus") {
     if (favSport === "bBall" ) {
       if (favHoliday === "christmas") {
-        $("#language").text("Ruby!");
+        $("#language").text("Ruby");
       } else if (favHoliday === "thanksgiving") {
-        $("#language").text("Python!");
+        $("#language").text("Python");
       } else if (favHoliday === "halloween") {
         $("#language").text("C#");
       }
     } else if (favSport === "fBall") {
       if (favHoliday === "christmas") {
-        $("#language").text("Ruby");
+        $("#language").text("C#");
       } else if (favHoliday === "thanksgiving") {
         $("#language").text("Python");
       } else  if (favHoliday === "halloween") {
-        $("#language").text("C#");
+        $("#language").text("Ruby");
       }
     } else if (favSport === "soccer") {
       if (favHoliday === 'christmas') {
         $("#language").text("Ruby");
       } else if (favHoliday === 'thanksgiving') {
-        $("#language").text("Python");
-      } else  if (favHoliday === "halloween") {
         $("#language").text("C#");
+      } else  if (favHoliday === "halloween") {
+        $("#language").text("Python");
       }
     }
   } else if (businessSize === "medBus") {
     if (favSport === "bBall") {
       if (favHoliday === "christmas") {
-        $("#language").text("Ruby");
-      } else if (favHoliday === "thanksgiving") {
         $("#language").text("Python");
+      } else if (favHoliday === "thanksgiving") {
+        $("#language").text("Ruby");
       } else if (favHoliday === "halloween") {
         $("#language").text("C#");
       }
     } else if (favSport === "fBall") {
       if (favHoliday === "christmas") {
-        $("#language").text("Holly Flax");
+        $("#language").text("Python");
       } else if (favHoliday === "thanksgiving") {
-        $("#language").text("Nellie Bertram");
+        $("#language").text("C#");
       } else if (favHoliday === "halloween") {
-        $("#language").text("Jan Levinson");
+        $("#language").text("Ruby");
       }
     } else if (favSport === "soccer") {
       if (favHoliday === "christmas") {
         $("#language").text("Ruby");
       } else if (favHoliday === "thanksgiving") {
-        $("#language").text("Python");
-      } else if (favHoliday === "halloween") {
         $("#language").text("C#");
+      } else if (favHoliday === "halloween") {
+        $("#language").text("Python");
       }
     }
   } else if (businessSize === "bigBus") {
     $("#language").text("C++")
   }
+  
   
 
 
